bd12e3a8973fd6d8809e7a66d914b89f2683c73b
caleb.cranney <caleb.cranney.app@gmail.com>
2022-03-27 18:33:23 -0600

    Completed section 14

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,7 @@ import requests
 import random
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 from backend.blockchain.blockchain import Blockchain
 from backend.wallet.wallet import Wallet
@@ -10,7 +11,10 @@ from backend.wallet.transaction import Transaction
 from backend.wallet.transaction_pool import TransactionPool
 from backend.pubsub import PubSub
 
+
+
 app = Flask(__name__)
+CORS(app, resources={r'/*': {'origins': 'http://localhost:3000'} })
 blockchain = Blockchain()
 wallet = Wallet(blockchain)
 transaction_pool = TransactionPool()
@@ -23,6 +27,17 @@ def default(): return 'Welcome to the blockchain'
 @app.route('/blockchain')
 def route_blockchain():
     return jsonify(blockchain.to_json())
+
+@app.route('/blockchain/range')
+def route_blockchain_range():
+    start = int(request.args.get('start'))
+    end = int(request.args.get('end'))
+
+    return jsonify(blockchain.to_json()[::-1][start:end])
+
+@app.route('/blockchain/length')
+def route_blockchain_length():
+    return jsonify(len(blockchain.chain))
 
 @app.route('/blockchain/mine')
 def route_blockchain_mine():
@@ -78,5 +93,12 @@ if os.environ.get('PEER') == 'True':
         print(f'\n -- Successfully syncronized the local chain.')
     except Exception as e:
         print(f'\n -- Error synchronizing the local chain: {e}')
+
+if os.environ.get('SEED_DATA') == 'True':
+    for i in range(10): blockchain.add_block([
+        Transaction(Wallet(), Wallet().address, random.randint(2,50)).to_json(),
+        Transaction(Wallet(), Wallet().address, random.randint(2,50)).to_json(),
+    ])
+
 print(PORT)
 app.run(port=PORT)
